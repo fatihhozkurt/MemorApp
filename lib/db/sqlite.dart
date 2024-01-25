9954a191f1d5ba546e62a2b3e11171bad5ffd422
Cengizhanyildiz14 <cengizhanyildiz.14@outlook.com>
2024-01-25 17:36:33 +0300

    Hatırlatmalar veritabanı tablolarına eklendi ve bağlantılar kuruldu.

---
+++ lib/db/sqlite.dart
@@ -0,0 +1,43 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/user.dart';
+
+class DatabaseHelper {
+  final databaseName = "notes.db";
+  String noteTable =
+      "CREATE TABLE notes (noteId INTEGER PRIMARY KEY AUTOINCREMENT, noteTitle TEXT NOT NULL, noteContent TEXT NOT NULL, createdAt TEXT DEFAULT CURRENT_TIMESTAMP)";
+
+
+  String users =
+      "create table users (usrId INTEGER PRIMARY KEY AUTOINCREMENT, usrName TEXT UNIQUE, usrPassword TEXT)";
+
+
+  Future<Database> initDB() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, databaseName);
+
+    return openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute(users);
+      await db.execute(noteTable);
+    });
+  }
+
+  Future<bool> login(Users user) async {
+    final Database db = await initDB();
+
+    var result = await db.rawQuery(
+        "select * from users where usrName = '${user.usrName}' AND usrPassword = '${user.usrPassword}'");
+    if (result.isNotEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<int> signup(Users user) async {
+    final Database db = await initDB();
+
+    return db.insert('users', user.toMap());
+  }
+
+}
